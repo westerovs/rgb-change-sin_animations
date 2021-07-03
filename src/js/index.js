2f5a9598ef3966ea1bad1168537e9749ef93eb98
Александр <westerovs@mail.ru>
2021-07-03 22:21:24 +0300

    refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,55 @@
+// цвета переведённые в rgb из hex
+// Пример
+// 196,196,196 - gray
+// 222,152,131 - color
+// =26 =44 =65
+let squares = document.querySelectorAll('.rect')
+
+const getDifColor = (firstColor = [], secondColor = []) => {
+    if (firstColor.length === 3 && secondColor.length === 3) {
+        return {
+            difR: (firstColor[0] > secondColor[0]) ? firstColor[0] - secondColor[0]  : secondColor[0] - firstColor[0],
+            difG: (firstColor[1] > secondColor[1]) ? firstColor[1] - secondColor[1]  : secondColor[1] - firstColor[1],
+            difB: (firstColor[2] > secondColor[2]) ? firstColor[2] - secondColor[2]  : secondColor[2] - firstColor[2],
+        }
+    }
+    
+    console.log('некорректный ввод')
+}
+
+const rgbDif = getDifColor([0,255,169], [255,221,0])
+
+console.log(rgbDif)
+
+const amplitude1 = rgbDif.difR;
+const amplitude2 = rgbDif.difG;
+const amplitude3 = rgbDif.difB;
+
+const startColor = [0,255,169]
+
+let count1 = 0
+let count2 = 0
+let count3 = 0
+
 let currentAnimationTime = 0;
 
-function runAnimate() {
-    // цвета переведённые в rgb из hex
-    // 196,196,196 - gray
-    // 222,152,131 - color
-    // =26 =44 =65
-    
-    let squares = document.querySelectorAll('.rect')
-    currentAnimationTime += 0.09;
+function convertColors() {
+    currentAnimationTime += 0.009;
     
     // amplitude = разница в значениях между верхней и нижней границей перехода цветов
-    const amplitude1 = 26;
-    const amplitude2 = 44;
-    const amplitude3 = 65;
-    
-    const startColor = 196
-    
-    let count1 = 0
-    let count2 = 0
-    let count3 = 0
-    
     squares.forEach((item, index) => {
-        count1 = startColor + Math.trunc(Math.sin(currentAnimationTime - index) * amplitude1)
-        count2 = startColor - Math.trunc(Math.sin(currentAnimationTime - index) * amplitude2)
-        count3 = startColor - Math.trunc(Math.sin(currentAnimationTime - index) * amplitude3)
+        count1 = startColor[0] + Math.trunc(Math.sin(currentAnimationTime - index) * amplitude1)
+        count2 = startColor[1] - Math.trunc(Math.sin(currentAnimationTime - index) * amplitude2)
+        count3 = startColor[2] - Math.trunc(Math.sin(currentAnimationTime - index) * amplitude3)
     
         item.style.backgroundColor = `rgb(
-            ${ (count1 <= startColor) ? startColor : count1 },
-            ${ (count2 >= startColor) ? startColor : count2 },
-            ${ (count3 >= startColor) ? startColor : count3 }
+            ${ (count1 <= startColor[0]) ? startColor[0] : count1 },
+            ${ (count2 >= startColor[1]) ? startColor[1] : count2 },
+            ${ (count3 >= startColor[2]) ? startColor[2] : count3 }
            )`
     })
     
-    requestAnimationFrame(runAnimate);
+    requestAnimationFrame(convertColors);
 }
 
-runAnimate();
+convertColors();
