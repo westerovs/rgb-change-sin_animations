0165661c3741538c6e6a5667e7d34af0204a40d3
Александр <westerovs@mail.ru>
2021-07-03 23:30:43 +0300

    refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,65 @@
-// Плавная смена цвета.
-// Принимает два rgb на вход
-// опционально альфаканал
+/*
+* Принимает два любых rgb на вход.
+*  Получает разницу между цветами и плавно меняет цвет от первого RGB до второго
+* В идеале, обернуть это в класс и добавить альфаканал ещё
+*/
 let squares = document.querySelectorAll('.rect')
 
-const getDifColor = (firstColor = [], secondColor = []) => {
-    if (firstColor.length === 3 && secondColor.length === 3) {
+const getDifColor = (firstRgb , secondRgb) => {
+    if (firstRgb.length === 3 && secondRgb.length === 3) {
         return {
-            startColor: firstColor,
-            difR: (firstColor[0] > secondColor[0]) ? firstColor[0] - secondColor[0]  : secondColor[0] - firstColor[0],
-            difG: (firstColor[1] > secondColor[1]) ? firstColor[1] - secondColor[1]  : secondColor[1] - firstColor[1],
-            difB: (firstColor[2] > secondColor[2]) ? firstColor[2] - secondColor[2]  : secondColor[2] - firstColor[2],
+            startColor: firstRgb,
+            // получаем разницу в значениях между верхней и нижней границей перехода цветов
+            difR: (firstRgb[0] > secondRgb[0]) ? firstRgb[0] - secondRgb[0] : secondRgb[0] - firstRgb[0],
+            difG: (firstRgb[1] > secondRgb[1]) ? firstRgb[1] - secondRgb[1] : secondRgb[1] - firstRgb[1],
+            difB: (firstRgb[2] > secondRgb[2]) ? firstRgb[2] - secondRgb[2] : secondRgb[2] - firstRgb[2],
         }
     }
-    
     console.log('некорректный ввод')
 }
 
-const rgbDif = getDifColor([0,255,169], [255,221,0])
-console.log(rgbDif)
-
-const amplitude1 = rgbDif.difR;
-const amplitude2 = rgbDif.difG;
-const amplitude3 = rgbDif.difB;
+const rgbDif = getDifColor([0, 255, 169], [255, 5, 38])
 
 const startColor = rgbDif.startColor
-
+const amplitude = {
+    r: rgbDif.difR,
+    g: rgbDif.difG,
+    b: rgbDif.difB,
+}
 const result = {
     r: 0,
     g: 0,
     b: 0,
 }
 
-let currentAnimationTime = 0;
+// играя с этим числом, можно получить интересные эффекты
+const typeAnimation = 5
 
-function convertColors() {
-    currentAnimationTime += 0.03;
+let currentAnimationTime = 0;
+let animationSquare = null
+
+function initConvertColors() {
+    currentAnimationTime += 0.005;
     
-    // amplitude = разница в значениях между верхней и нижней границей перехода цветов
     squares.forEach((item, index) => {
-        result.r = startColor[0] + Math.trunc(Math.sin(currentAnimationTime - index) * amplitude1)
-        result.g = startColor[1] - Math.trunc(Math.sin(currentAnimationTime - index) * amplitude2)
-        result.b = startColor[2] - Math.trunc(Math.sin(currentAnimationTime - index) * amplitude3)
+        result.r = startColor[0] + Math.trunc(Math.sin(currentAnimationTime - index / typeAnimation) * amplitude.r)
+        result.g = startColor[1] - Math.trunc(Math.sin(currentAnimationTime - index / typeAnimation) * amplitude.g)
+        result.b = startColor[2] - Math.trunc(Math.sin(currentAnimationTime - index / typeAnimation) * amplitude.b)
     
         item.style.backgroundColor = `rgb(
             ${ (result.r <= startColor[0]) ? startColor[0] : result.r },
             ${ (result.g >= startColor[1]) ? startColor[1] : result.g },
             ${ (result.b >= startColor[2]) ? startColor[2] : result.b }
-           )`
+        )`
     })
     
-    requestAnimationFrame(convertColors);
+    animationSquare = requestAnimationFrame(initConvertColors);
 }
 
-convertColors();
+const destroyConvertColors = () => {
+    currentAnimationTime = 0
+    cancelAnimationFrame(animationSquare)
+}
+
+initConvertColors();
+
